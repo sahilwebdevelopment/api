95ea18f2980e8e83faad9b3c924f05f063ccf94d
Sahil <sahilchandwani347@gmail.com>
2024-10-30 19:44:05 +0530

    helllo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { promises as fs } from 'fs'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body
+    const hi = await fs.readFile('public/hi.json', 'UTF-8');
+    const ob = JSON.parse(hi)
+    ob.push(data)
+    const ids = JSON.stringify(ob)
+    await fs.writeFile("public/hi.json", ids);
+    return res.json(data);
+}else{
+  const hi = await fs.readFile('public/hi.json', 'UTF-8');
+  const ob = JSON.parse(hi)
+  res.status(200).json(ob);
+}
 }
