5ed3c8adc8c5d8ccb0a3c1117acdd4ca7632d97a
sahilwebdevelopment <141914983+sahilwebdevelopment@users.noreply.github.com>
2024-10-30 19:50:39 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,14 +5,14 @@ import { promises as fs } from 'fs'
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body
-    const hi = await fs.readFile('public/hi.json', 'UTF-8');
+    const hi = await fs.readFile('hi.json', 'UTF-8');
     const ob = JSON.parse(hi)
     ob.push(data)
     const ids = JSON.stringify(ob)
-    await fs.writeFile("public/hi.json", ids);
+    await fs.writeFile("hi.json", ids);
     return res.json(data);
 }else{
-  const hi = await fs.readFile('public/hi.json', 'UTF-8');
+  const hi = await fs.readFile('hi.json', 'UTF-8');
   const ob = JSON.parse(hi)
   res.status(200).json(ob);
 }
